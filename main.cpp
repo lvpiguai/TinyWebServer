93171cae4b0a90ba859908a88dec5783f74ed17d
lvpiguai <2916717542@qq.com>
2026-01-21 21:30:40 +0800

    新增 epoll 机制

--- main.cpp
+++ main.cpp
@@ -4,33 +4,81 @@
 #include <cstring>
 #include<pthread.h>
 #include "thread_pool.h"
+#include <sys/epoll.h>
+#include <fcntl.h>
 
 using namespace std;
+
+constexpr int MAX_EVENT_NUMBER = 10000;
+
+//设置socket 非阻塞
+void setnonblocking(int fd)
+{
+    int oldoption = fcntl(fd,F_GETFL);
+    int newoption  = oldoption | O_NONBLOCK;
+    fcntl(fd,F_SETFL,newoption);
+
+}
+
+//添加 fd 到 epoll 监控列表
+void addfd(int epollfd,int fd,bool one_shot)
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;//新连接，新数据，断连接，ET 模式
+    if(one_shot)event.events |= EPOLLONESHOT; 
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&event); //加入列表需要说明需要监听的事件
+    setnonblocking(fd); //使用 epoll ，socket 相关函数必须非阻塞，只需要 epoll_wait 一个阻塞即一个监听
+}
 
 int main(int argc, char const *argv[])
 {
     //创建socket
-    int fd = socket(AF_INET,SOCK_STREAM,0);
+    int listenfd = socket(AF_INET,SOCK_STREAM,0);
     //创建地址
     struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr =  htonl(INADDR_ANY);
     addr.sin_port = htons(9000);
+    //避免端口锁定
+    int reuse = 1;
+    setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&reuse,sizeof(reuse));
     //给 socket 绑定地址
-    bind(fd,(const sockaddr*)&addr,sizeof(addr));
+    bind(listenfd,(const sockaddr*)&addr,sizeof(addr));
     //开启监听
-    listen(fd,5);
+    listen(listenfd,5);
+    //创建epoll 实例
+    int epollfd = epoll_create(5);
+    addfd(epollfd,listenfd,false); // 监听socket 不用 ONESHOT 否则只能
+    epoll_event event[MAX_EVENT_NUMBER];
     //创建线程池
-    ThreadPool thread_pool;
+    ThreadPool thread_pool(epollfd);
     //死循环，处理新连接     
     while(1)
     {
-        //阻塞等待连接
-        int confd = accept(fd,nullptr,nullptr);
-        thread_pool.append(confd);
+        //epoll 阻塞监听所有 fd
+        int num = epoll_wait(epollfd,event,MAX_EVENT_NUMBER,-1);
+        //遍历处理所有事件
+        for(int i = 0;i<num;++i)
+        {
+            int fd = event[i].data.fd;
+            if(fd==listenfd){
+                while(true){
+                    int confd = accept(listenfd,nullptr,nullptr);
+                    if(confd<0){
+                        if(errno!=EAGAIN && errno!=EWOULDBLOCK)perror("accept error");
+                        break;
+                    }
+                    addfd(epollfd,confd,true); //加入监听链表
+                }
+                
+            }else{
+                thread_pool.append(fd); //追加到任务列表
+            }
+        }
     }
-    //关闭监听fd
-    close(fd);
-
+    //关闭 listenfd 和 epollfd
+    close(listenfd);
+    close(epollfd);
     return 0;
 }
--- thread_pool.h
+++ thread_pool.h
@@ -5,17 +5,14 @@
 #include <iostream>
 #include<unistd.h>
 #include <cstring>
+#include <sys/epoll.h>
 
 using namespace std;
 
 class ThreadPool{
-private:
-    pthread_mutex_t lock; //互吹锁
-    pthread_cond_t cond; //条件变量
-    queue<int> task_queue; //任务队列
 public:
     //构造
-    ThreadPool(int num = 5){
+    ThreadPool(int epollfd,int num = 5):epollfd(epollfd){
         //初始化
         pthread_mutex_init(&lock,nullptr);
         pthread_cond_init(&cond,nullptr);
@@ -31,22 +28,27 @@ public:
         pthread_mutex_destroy(&lock); 
         pthread_cond_destroy(&cond);
     }
-
+    //追加任务
+    void append(int fd){
+        pthread_mutex_lock(&lock);//加锁
+        task_queue.push(fd);//加入队列
+        pthread_mutex_unlock(&lock);//开锁
+        pthread_cond_signal(&cond); //唤醒
+    }
+private:
     //静态 worker 函数（创建线程必须传递全局函数）
     static void* worker(void* arg){
         ThreadPool* pool = (ThreadPool*)arg;
         pool->run();
         return nullptr;
     }
-
-    //死循环处理连接
+    //线程实际处理函数
     void run(){
-        while(1){ //死循环处理任务队列
+        while(true){ //死循环处理任务队列
             pthread_mutex_lock(&lock);//加锁
             while(task_queue.empty()){ 
                 pthread_cond_wait(&cond,&lock); //释放锁，挂起线程
             }
-            
             //取出任务 fd 
             int confd = task_queue.front();
             task_queue.pop();
@@ -56,27 +58,42 @@ public:
             cout<<"tid = "<<tid<<endl;
             //读
             char buf[1024] = {0};
-            read(confd,buf,sizeof(buf));
-            //cout<<buf<<endl; //输出浏览器发送的信息
-
-            //创建响应
-            const char* response =  
-                "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/html; charset=utf-8\r\n"
-                "Content-Length: 19\r\n"
-                "\r\n"
-                "<h1>Hello World</h1>";
-            //写
-            write(confd,response,strlen(response));
+            bool close_con = false;
+            while(true){
+                int len = read(confd,buf,sizeof(buf));
+                if(len>0){
+                    cout<<"收到数据："<<buf<<endl;
+                }else if(len==0){
+                    close_con = true;
+                }else{
+                    if(errno!=EAGAIN && errno!=EWOULDBLOCK)close_con = true; // 出错就关闭
+                    break; //读完就退出
+                }
+            }
             //关闭
-            close(confd);
+            if(close_con){
+                close(confd);
+                cout<<"客户端关闭：confd = "<<confd<<endl;
+            }else{
+                 //写 
+                const char* response = "HTTP/1.1 200 OK\r\nContent-Length: 19\r\n\r\n<h1>Hello World</h1>";
+                write(confd,response,strlen(response));
+                //重置 ONESHOT
+                reset_oneshot(confd);
+            }
         }
     }
-    //追加任务
-    void append(int fd){
-        pthread_mutex_lock(&lock);//加锁
-        task_queue.push(fd);//加入队列
-        pthread_mutex_unlock(&lock);//开锁
-        pthread_cond_signal(&cond); //唤醒
+    void reset_oneshot(int fd){
+        epoll_event event;
+        event.data.fd = fd;
+        event.events = EPOLLIN | EPOLLRDHUP | EPOLLET | EPOLLONESHOT;
+        epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&event);
     }
+
+private:
+    pthread_mutex_t lock; //互吹锁
+    pthread_cond_t cond; //条件变量
+    queue<int> task_queue; //任务队列
+    int epollfd; //存 epoll 实例
+
 };
